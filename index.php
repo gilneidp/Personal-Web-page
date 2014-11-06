99e9499e05b8579432ad80eb3dbe6654a241af7e
gilneidp <gilnei@gilnei.com.br>
2014-11-06 17:00:44 -0600

    l

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 	
 	<div class="col-md-9">
     <div class="row">	
-    <div class="col-md-6">
+    <div class="col-md-12">
 	<h2>This site is being constructed. Come back again soon! </h2>
 	</div>
         </div>
