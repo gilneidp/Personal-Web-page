5d8d0f5fb860923c737b5d8e25d0c4c61d15ebf3
gilneidp <gilnei@gilnei.com.br>
2014-11-06 16:23:21 -0600

    Update main.php

--- main.php
+++ main.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="css/bootstrap-responsive.css" rel="stylesheet"> 
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
     <title>GilneiDP</title>
 
     <!-- Bootstrap -->
