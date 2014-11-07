01ae542b0401c47822cf7845d10684a3c752cbeb
gilneidp <gilnei@gilnei.com.br>
2014-11-06 19:11:56 -0600

    Update main.php

--- main.php
+++ main.php
@@ -4,8 +4,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="css/bootstrap-responsive.css" rel="stylesheet"> 
-	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
     <title>GilneiDP</title>
 
     <!-- Bootstrap -->
@@ -17,20 +15,29 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-	 <link rel="stylesheet" href="css/personal.css">
-	 <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    <link rel="stylesheet" href="css/personal.css">
   </head>
   <body>
+  
   <div class="container main-content">
   <div class="row">
+ 
   <div class="col-md-12">
       <a href="#"><img class="head-image" src="img/1.jpg" alt="GilneiDP"></a>
     </div>
-	
-	
-    <h1> Building UP!</h1>
-     </div>
 	</div>
+	
+	<div class="col-md-9">
+    <div class="row">
+    <div class="col-md-12">
+	<h2>This site is being constructed. Come back again soon! </h2>
+	</div>
+        </div>
+      </div>
+	</div>
+	
+<body>
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
