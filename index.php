36e36619474023b8523da7ed3ce368f3154e82c0
gilneidp <gilnei@gilnei.com.br>
2014-11-07 10:10:30 -0600

    1

--- index.php
+++ index.php
@@ -18,6 +18,10 @@
     <link rel="stylesheet" href="css/personal.css">
   </head>
   <body>
+   <div class="row">
+      <div id="navigation-menu" class="col-md-12 log-siderbar">
+      </div>
+   </div>
   
   <div class="container main-content">
   <div class="row">
@@ -41,6 +45,7 @@
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
+	 <script src="js/main.js"></script>
     <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
