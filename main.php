ef06bb715b00f21ec4940d5f47df2f336b2add58
gilneidp <gilnei@gilnei.com.br>
2014-11-07 16:15:33 -0600

    36

--- main.php
+++ main.php
@@ -44,11 +44,12 @@
 	   <p class="bg-primary list-group">Education and Experience</p>
 	    </div>
 	    <div class="col-md-8 edu-background">
-	    <a href="fw.uri.br"><img src="img/uri.jpg" alt="MySchool" class="image-com"></a>
-		<a href="www.ubnt.com"><img src="img/ubnt.png" alt="Myknown" class="image-com"></a>
-		<a href="www.atuanet.com.br"><img src="img/atua.jpg" alt="Myjob" class="image-com"></a>
-	    <a href="#"><img src="img/ksu.jpg" alt="Mycolege" class="image-com"></a>
+	    <a href="http://fw.uri.br"><img src="img/uri.jpg" alt="MySchool" class="image-com"></a>
+		<a href="https://ubnt.com"><img src="img/ubnt.png" alt="Myknown" class="image-com"></a>
+		<a href="http://atuanet.com.br"><img src="img/atua.jpg" alt="Myjob" class="image-com"></a>
+	    <a href="http://ksu.edu"><img src="img/ksu.jpg" alt="Mycolege" class="image-com"></a>
 	    <a href="#"><img src="img/lancore.png" alt="MyTraining" class="image-com"></a>
+		<a href="http://mikrotik.com"><img src="img/mikrotik.png" alt="MyTraining1" class="image-com"></a>
 	   </div>
 	 </div>
 	</div>
