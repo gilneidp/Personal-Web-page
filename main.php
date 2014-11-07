8563616d2dac0c13728a4d8c5e1aff6092a73d9e
gilneidp <gilnei@gilnei.com.br>
2014-11-07 11:24:28 -0600

    18

--- main.php
+++ main.php
@@ -30,7 +30,7 @@
      <div class="row">
 	   <div class="col-md-4">
 	    <div class="align-center">
-		 <a href="#"><img class="img-circle" class="profile-pic" src="img/profile.jpg" alt="Gilnei De Pellegrin"></a>
+		 <a href="#"><img src="img/profile.jpg" alt="Gilnei De Pellegrin" class="profile-pic"></a>
 		</div>
 	   </div>
 	   <div class="col-md-8">
